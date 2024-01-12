4b75ce0cc0243b23a69dfefad15d09a56c63b458
viktor d <10xl.pro@mail.ru>
2024-01-12 20:29:01 +0300

    Добавлены пустые списки по умолчанию

--- Airports/AddedDb.cs
+++ Airports/AddedDb.cs
@@ -4,7 +4,7 @@ namespace AirportsAndFlights
 {
     public class AddedDb
     {
-        public List<Airport> AddedAirports { get; set; } // Добавленные аэропорты
-        public List<Flight> AddedFlights { get; set; } // Добавленные рейсы
+        public List<Airport> AddedAirports { get; set; } = new List<Airport> { }; // Добавленные аэропорты
+        public List<Flight> AddedFlights { get; set; } = new List<Flight> { }; // Добавленные рейсы
     }
 }
--- Airports/LocalDb.cs
+++ Airports/LocalDb.cs
@@ -2,10 +2,10 @@
     
 namespace AirportsAndFlights
 {
-    
+
     public class LocalDb // Локальная база данных
     {
-        public List<Airport> Airports { get; set; } // Аэропорты
-        public List<Flight> Flights { get; set; } // Рейсы
+        public List<Airport> Airports { get; set; } = new List<Airport> { }; // Аэропорты
+        public List<Flight> Flights { get; set; } = new List<Flight> { }; // Рейсы
     }
 }
