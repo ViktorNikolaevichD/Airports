d5df33f88709118ab72c7e1ec0e2091189bd23bf
viktor d <10xl.pro@mail.ru>
2024-01-12 20:28:45 +0300

    Добавлена генерация аэропортов и рейсов

--- Airports/Commands.cs
+++ Airports/Commands.cs
@@ -1,9 +1,42 @@
 ﻿using AirportsAndFlights.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 namespace AirportsAndFlights
 {
     public class Commands
     {
+        // Функция загрузки базы данных
+        public static LocalDb LoadingDb(int rank, int size)
+        {
+            using (var db = new AppDbContext())
+            {
+                // Количество строк в каждой таблице
+                //int countAirport = db.Airports.Count();
+                int countFlight = db.Flights.Count();
+                // Размер части для каждой таблицы
+                //int partAirpot = (countAirport / size + 1);
+                int partFlight = (countFlight / size + 1);
+                // Смещение по каждой таблице
+                //int offsetAirport = rank * partAirpot;
+                int offsetFlight = rank * partFlight;
+
+                // Вернуть локальную базу данных
+                return new LocalDb
+                {
+                    // Аэропорты (у каждой локальной базы есть список полный список аэропортов)
+                    Airports = db.Airports
+                            .ToList(),
+                    // Рейсы
+                    Flights = db.Flights
+                            .Include(u => u.DepartureAirport)
+                            .Include(u => u.ArrivalAirport)
+                            .Skip(offsetFlight)
+                            .Take(partFlight)
+                            .ToList()
+                };
+            }
+        }
         // Генерация аэропортов
         public static void GenerateAirports(int count)
         {
@@ -11,12 +44,104 @@ namespace AirportsAndFlights
             {
                 for (int i = 0; i < count; i++)
                 {
-                    db.Add(new Airport
+                    db.Airports.Add(new Airport
                     {
-
-                    })
+                        Name = Faker.Internet.DomainWord(),
+                        City = Faker.Address.City(),
+                        Status = "Открыт"
+                    });
                 }
+                // Сохранить
+                db.SaveChanges();
             }
         }
+
+        // Функция для определения статуса рейса
+        public static string CheckFlightStatus(DateTime depTime, DateTime arrivTime, int depAirportId, int arrivAirportId)
+        {
+            string status = "Неопределен";
+
+            if (depTime == arrivTime)
+            {
+                status = "Отменен";
+                return status;
+            }
+            if (depAirportId == arrivAirportId)
+            {
+                status = "Возвращен";
+                return status;
+            }    
+            if (depTime < DateTime.Now &&
+                arrivTime <= DateTime.Now)
+            {
+                status = "Завершен";
+                return status;
+            }
+            if (depTime <= DateTime.Now &&
+                arrivTime > DateTime.Now)
+            {
+                status = "Выполняется";
+                return status;
+            }
+            if (depTime > DateTime.Now &&
+                arrivTime > DateTime.Now )
+            {
+                status = "Запланирован";
+                return status;
+            }
+
+            return status;
+        }
+        // Генерация рейсов
+        public static void GenerateFlights(int count)
+        {
+            using (var db = new AppDbContext())
+            {
+                // Список аэропортов
+                List<Airport> airports = db.Airports.Where(u => u.Status == "Открыт").ToList();
+                for (int i = 0; i < count;i++)
+                {
+                    // Аэропорт вылета
+                    int departuerAirportId = airports[Faker.RandomNumber.Next(0, airports.Count() - 1)].Id;
+                    // Аэропорт прилета
+                    int arrivalAirportId = airports[Faker.RandomNumber.Next(0, airports.Count() - 1)].Id;
+                    // Время вылета самолета
+                    DateTime departureTime = new DateTime(
+                        DateTime.Now.Year,
+                        Faker.RandomNumber.Next(1, 12),
+                        Faker.RandomNumber.Next(1, 28),
+                        Faker.RandomNumber.Next(0, 23),
+                        Faker.RandomNumber.Next(0, 3) * 15,
+                        0);
+                    // Время прилета самолета
+                    DateTime arrivalTime = departureTime
+                        .AddHours(Faker.RandomNumber.Next(0, 9))
+                        .AddMinutes(Faker.RandomNumber.Next(0, 3) * 15);
+
+                    // Если время вылета и прилета совпадает, то изменить аэропорт прилета
+                    if (departureTime == arrivalTime)
+                    {
+                        arrivalAirportId = departuerAirportId;
+                    }
+
+                    // Статус рейса
+                    string status = CheckFlightStatus(departureTime, arrivalTime, departuerAirportId, arrivalAirportId);
+                        
+                    db.Flights.Add(new Flight
+                    {
+                        DepartureAirportId = departuerAirportId,
+                        ArrivalAirportId = arrivalAirportId,
+                        DepartureTime = departureTime,
+                        ArrivalTime = arrivalTime,
+                        Status = status
+                        
+                    });
+                }
+                // Сохранить
+                db.SaveChanges();
+            }
+        }
+        
+        // 
     }
 }
--- Airports/Program.cs
+++ Airports/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AirportsAndFlights
+﻿using System.Diagnostics;
+
+namespace AirportsAndFlights
 {
     public class Program
     {
@@ -6,6 +8,138 @@
         {
             MPI.Environment.Run(ref args, comm =>
             {
+                // Локальная база для добавленных объектов
+                AddedDb AddedDb = new AddedDb();
+                // Локальная база
+                LocalDb localDb = new LocalDb();
+
+                // Предзагрузка базы данных
+                // Первым загружает базу 0 процесс, чтобы не было проблемы с одновременным созданием базы данных
+                if (comm.Rank == 0)
+                    // База данных для 0 процесса
+                    localDb = Commands.LoadingDb(comm.Rank, comm.Size);
+                // После того как 0 процесс загрузит базу, то все процессы выйдут из барьера
+                comm.Barrier();
+                // Все !0 процессы загрузят базу
+                if (comm.Rank != 0)
+                    // База данных для остальных(не 0) процессов
+                    localDb = Commands.LoadingDb(comm.Rank, comm.Size);
+
+                // Замер времени работы
+                Stopwatch stopWatch = new Stopwatch();
+                // Команда пользователя
+                string? command = null;
+
+                while (command != "quit")
+                {
+                    // Получение команды от пользователя
+                    if (comm.Rank == 0)
+                    {
+                        Console.Write("Введите команду \ngenAirport - сгенерировать аэропорты;" +
+                                                      "\ngenFlight - сгенерировать рейсы; " +
+
+                                                      "\nquit - выйти из программы: ");
+                        command = Console.ReadLine();
+                    }
+
+                    // Рассылка команды по всем процессам
+                    comm.Broadcast(ref command, 0);
+
+                    switch (command)
+                    {
+                        case "genAirport":
+                            // Число строк для генерации
+                            int count = 0;
+                            // Число строк для генерации каждым процессом
+                            int countForRank = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Сколько аэропортов сгенерировать: ");
+                                count = Convert.ToInt32(Console.ReadLine());
+                                stopWatch.Restart();
+                                countForRank = count / comm.Size;
+                            }
+
+                            // Разослать всем процессам количество строк для генерации
+                            comm.Broadcast(ref countForRank, 0);
+                            
+                            if (comm.Rank == 0)
+                            {
+                                // Добавить недостающие строчки
+                                countForRank = countForRank + (count % comm.Size);
+                            }
+                            
+                            
+                            // Генерируем аэропорты
+                            Commands.GenerateAirports(countForRank);
+
+                            // Все процессы ждут окончания генерации
+                            comm.Barrier();
+
+                            // Обновление локальной базы данных
+                            localDb = Commands.LoadingDb(comm.Rank, comm.Size);
+
+                            if (comm.Rank == 0)
+                            {
+                                stopWatch.Stop();
+                                Console.WriteLine("Аэропорты сгенерированы");
+                            }
+                            break;
+                        case "genFlight":
+                            // Число строк для генерации
+                            count = 0;
+                            // Число строк для генерации каждым процессом
+                            countForRank = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Сколько аэропортов сгенерировать: ");
+                                count = Convert.ToInt32(Console.ReadLine());
+                                stopWatch.Restart();
+                                countForRank = count / comm.Size;
+                            }
+
+                            // Разослать всем процессам количество строк для генерации
+                            comm.Broadcast(ref countForRank, 0);
+
+                            if (comm.Rank == 0)
+                            {
+                                // Добавить недостающие строчки
+                                countForRank = countForRank + (count % comm.Size);
+                            }
+
+
+                            // Генерируем аэропорты
+                            Commands.GenerateFlights(countForRank);
+
+                            // Все процессы ждут окончания генерации
+                            comm.Barrier();
+
+                            // Обновление локальной базы данных
+                            localDb = Commands.LoadingDb(comm.Rank, comm.Size);
+
+                            if (comm.Rank == 0)
+                            {
+                                stopWatch.Stop();
+                                Console.WriteLine("Аэропорты сгенерированы");
+                            }
+                            break;
+                        default:
+                            if (comm.Rank == 0 && command != "quit")
+                                Console.WriteLine("Неизвестная команда");
+                            break;
+                    }
+                    if (comm.Rank == 0)
+                    {
+                        // Вывод времени выполнения
+                        TimeSpan ts = stopWatch.Elapsed;
+                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                        ts.Hours, ts.Minutes, ts.Seconds,
+                        ts.Milliseconds / 10);
+                        Console.WriteLine($"RunTime {comm.Rank} " + elapsedTime);
+                    }
+                    // Барьер, чтобы все процессы подождали, пока 0 процесс выведет время работы
+                    comm.Barrier();
+                }
             });
         }
     }
